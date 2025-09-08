1ac8c9f823aa25ab1641f628f4f2377d313995f4
Maddie Effa <meffa2005@gmail.com>
2025-09-07 20:37:41 -0500

    second push-homepage setup. Updated website.

--- index.php
+++ index.php
@@ -6,8 +6,24 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Maddie's Site</title>
+    <link rel="stylesheet" type="text/css" href="css/base.css">
 </head>
 <body>
-<h1>Hello World</h1>
+<header><h1>PHP Class - Maddie's Website</h1></header>
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#">Loops</a></li>
+        <li><a href="#">Countdown Timer</a></li>
+        <li><a href="#">Other</a></li>
+    </ul>
+</nav>
+<main>
+    <img src="image/HomeImage.jpg" alt="">
+    <p>This is my home page for my website. It is styled using a css file. <br>This is my second year of college. I am excited to take PHP. Here is what lorem looks like: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda commodi consequuntur eligendi eum facere laborum omnis quae quibusdam unde.</p>
+</main>
+<footer>
+    <p>&copy; 2025 MaddieEffa.com</p>
+</footer>
 </body>
 </html>
