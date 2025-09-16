7d8c8451d565a5850cfcc6cd735ab4e23fbacedb
Maddie Effa <meffa2005@gmail.com>
2025-09-15 19:22:40 -0500

    Fourth Commit. Countdown file creation

---
+++ countdown/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Maddie's Site</title>
+    <link rel="stylesheet" type="text/css" href="/css/base.css">
+</head>
+<body>
+<header><?php include '../includes/header.php';?></header>
+<nav>
+    <?php include '../includes/nav.php';?>
+</nav>
+<main>
+
+</main>
+<footer>
+    <?php include '../includes/footer.php';?>
+</footer>
+</body>
+</html>
+
