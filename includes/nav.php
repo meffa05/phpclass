1ba452d755f86fdccd62507fb42b1342b401186b
Maddie Effa <meffa2005@gmail.com>
2025-09-22 17:30:22 -0500

    Fifth Commit. Class work

--- includes/nav.php
+++ includes/nav.php
@@ -3,8 +3,8 @@ $isLoops = $_SERVER['REQUEST_URL] =='/';
 $isCountdown = $_SERVER['REQUEST_URL] =='/';
 
 <ul>
-    <li> class= <a href="/">Home</a></li>
-    <li><a href="#">Loops</a></li>
+    <li><a href="/">Home</a></li>
+    <li><a href="/loops">Loops</a></li>
     <li><a href="#">Countdown Timer</a></li>
     <li><a href="#">Other</a></li>
 </ul>
