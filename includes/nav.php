625f1db79cff5238b468a1893546ae02420b7e1c
Maddie Effa <meffa2005@gmail.com>
2025-10-13 19:41:06 -0500

    thirteenth. updated nav

--- includes/nav.php
+++ includes/nav.php
@@ -4,11 +4,13 @@ $isloops = $_SERVER['REQUEST_URL'] =='/loops/'  ? 'selected' : '';
 $iscountdown = $_SERVER['REQUEST_URL'] =='/countdown/' ? 'selected' : '';
 $isMagic8Ball = $_SERVER['REQUEST_URL'] =='/Magic8Ball/' ? 'selected' : '';
 $ismovielist = $_SERVER['REQUEST_URL'] =='/movielist/' ? 'selected' : '';
+$iscustlist = $_SERVER['REQUEST_URL'] =='/custlist/' ? 'selected' : '';
 ?>
 <ul>
     <li><a href="/index.php">Home</a></li>
-    <li><a href="/loops">Loops</a></li>
-    <li><a href="/countdown">Countdown Timer</a></li>
+    <li><a href="/loops">Loops</a><?=$isloops?></li>
+    <li><a href="/countdown">Countdown Timer</a><?=$iscountdown?></li>
     <li><a href="/Magic8Ball">Magic 8 Ball</a><?=$isMagic8Ball?></li>
     <li><a href="/movielist">Movie List</a><?=$ismovielist?></li>
+    <li><a href="/custlist">Customer List</a><?=$iscustlist?></li>
 </ul>
