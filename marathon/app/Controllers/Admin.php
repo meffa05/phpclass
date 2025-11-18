62dee347688999bfa2159abb9725e389e07e9a4a
Maddie Effa <meffa2005@gmail.com>
2025-11-17 18:12:14 -0600

    24th.  Public Website Assignment: Marathon Views Addition

--- marathon/app/Controllers/Admin.php
+++ marathon/app/Controllers/Admin.php
@@ -6,7 +6,28 @@ class Admin extends BaseController
 {
     public function index(): string
     {
-        return view('admin_page');
+        $data=['index' => 'true'];
+        return view("admin_page",$data);
+    }
+    public function manage_marathon(): string //added from week 13 in class work
+    {
+        $data=['manage_marathon' => 'true'];
+        return view("marathon_page",$data);
+    }
+    public function add_marathon(): string //added from week 13 in class work
+    {
+        $data=['add_marathon' => 'true'];
+        return view("add_page",$data);
+    }
+    public function manage_runners(): string //added from week 13 in class work
+    {
+        $data=['manage_runners' => 'true'];
+        return view("runners_page",$data);
+    }
+    public function registration_page(): string //added from week 13 in class work
+    {
+        $data=['registration_page' => 'true'];
+        return view("registration_page",$data);
     }
 
 }
